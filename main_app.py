c08a9b7167505c1b92c24f9d269c75259f9dd621
AkhilG5 <167869960+AkhilG5@users.noreply.github.com>
2026-02-28 17:26:18 +0530

    3files

--- main_app.py
+++ main_app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Akhil Good Morning"
+    return render_template("index.html")
 
 @app.route("/health")
 def health():
@@ -12,9 +12,13 @@ def health():
 
 @app.route("/echo", methods=["POST"])
 def echo():
-    data = request.json
+    name = request.form.get("name")
+
+    if not name:
+        return jsonify({"error": "Name is required"}), 400
+
     return jsonify({
-        "received": data
+        "message": f"Successfully done, {name}!"
     }), 200
 
 if __name__ == "__main__":
