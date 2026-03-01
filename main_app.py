f11045163c43cb9953e082f35e779c4949c491da
AkhilG5 <167869960+AkhilG5@users.noreply.github.com>
2026-03-01 17:37:26 +0530

    all

--- main_app.py
+++ main_app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+# @app.route("/homepage")
+# def homepage():
+#     return render_template("homepage.html")
+
 @app.route("/health")
 def health():
     return jsonify({"status": "ok"}), 200
@@ -13,13 +17,19 @@ def health():
 @app.route("/echo", methods=["POST"])
 def echo():
     name = request.form.get("name")
+    password = request.form.get("password")
+    
+    if not name or not password:
+        return "Name is required",400
+    return render_template("homepage.html",name=name)
 
-    if not name:
-        return jsonify({"error": "Name is required"}), 400
-
-    return jsonify({
-        "message": f"Successfully done, {name}!"
-    }), 200
+    # if not name:
+    #     return jsonify({"error": "Name is required"}), 400
+    # def homepage():
+    #     return render_template("homepage.html"),jsonify({
+    #     "message": f"Successfully done, {name}!"
+    # }), 200
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
