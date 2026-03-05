d3496ca0cc0cd25088b6114b2e017107e076b66f
AkhilG5 <167869960+AkhilG5@users.noreply.github.com>
2026-03-05 19:40:38 +0530

    new

--- main_app.py
+++ main_app.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify, request, render_template
+import psycopg2
 
 app = Flask(__name__)
 
+conn = psycopg2.connect(
+    host="localhost",
+    database="myfirstdb",
+    user="postgres",
+    password="root",
+    port=5432
+)
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",error=None)
 
 # @app.route("/homepage")
 # def homepage():
@@ -14,22 +23,36 @@ def home():
 def health():
     return jsonify({"status": "ok"}), 200
 
-@app.route("/echo", methods=["POST"])
-def echo():
-    name = request.form.get("name")
-    password = request.form.get("password")
-    
-    if not name or not password:
-        return "Name is required",400
-    return render_template("homepage.html",name=name)
+@app.route("/login", methods=["POST"])
+def login():
 
-    # if not name:
-    #     return jsonify({"error": "Name is required"}), 400
-    # def homepage():
-    #     return render_template("homepage.html"),jsonify({
-    #     "message": f"Successfully done, {name}!"
-    # }), 200
+    name = request.form.get("username")
+    password = request.form.get("password")
+
+    if not name or not password:
+        return "Username and Password required", 400
+
+    cur = conn.cursor()
+
+    query = """
+    SELECT * FROM users_login
+    WHERE username=%s AND password=%s
+    """
+
+    cur.execute(query, (name, password))
+    user = cur.fetchone()
+    cur.close()
+
+    if user:
+        return render_template("homepage.html", name=name)
+    else:
+        return render_template("index.html",error = "Invalid username or password")
+
     
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
